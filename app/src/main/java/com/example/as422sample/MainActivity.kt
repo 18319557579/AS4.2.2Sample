6fe6deed693b4e11fea0cba9248f1649a34df25e
ALittleDaisy <1152594956@qq.com>
2023-11-21 18:33:27 +0800

    子模块增加Activity，App建立与子模块的连接

--- app/src/main/java/com/example/as422sample/MainActivity.kt
+++ app/src/main/java/com/example/as422sample/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.as422sample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.childmodule.ChildMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, ChildMainActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ childmodule/src/main/java/com/example/childmodule/ChildMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.childmodule;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChildMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_main);
+    }
+}
